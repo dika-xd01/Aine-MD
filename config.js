da251fa49e5f5495b8c796aafa701d4a948e83f6
dika-xd01 <120255980+dika-xd01@users.noreply.github.com>
2022-12-10 19:40:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
+  ['6283123942188'],
+  ['6281227434223'],
   ['62895330379186', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
